73b51ecf6b48cc0f7892e38884d4e9e669a101df
kamitoshi <kroo.aq.74@gmail.com>
2020-07-06 16:03:02 +0900

    swiftUI tutorial

--- calcApp/ContentView.swift
+++ calcApp/ContentView.swift
@@ -10,11 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Text("消費税計算")
-            Calculator()
-        
+        VStack(alignment: .leading) {
+            Text("株式会社　bim")
+                .font(.title)
+            HStack {
+                Text("アイデアを形にする会社")
+                    .font(.subheadline)
+                Spacer()
+                Text("")
+                    .font(.subheadline)
+            }
         }
+        .padding()
     }
 }
 
@@ -22,32 +29,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct Calculator: View{
-    
-    @State var price = ""
-    @State var tax = ""
-    @State var total = 0.0
-    @State var final = 0
-    
-    var body: some View{
-        Form{
-            Section(header: Text("食費")){
-                
-                TextField("価格", text: self.$price).keyboardType(.numberPad)
-                TextField("消費税", text: self.$tax).keyboardType(.numberPad)
-                
-                Button(action: {
-                    self.total = Double(self.price)! + (Double(self.price)! * Double(self.tax)! / 100.0)
-                    self.final = Int(self.total)
-                }){
-                    Text("確定")
-                }
-                Text("\(self.final)円")
-            }
-        }
-    }
-    
-    
 }
